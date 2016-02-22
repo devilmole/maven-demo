8a0889635445580360f5b5ea1383d44e157fa13d
devilmole <devilmole@gmail.com>
2016-02-22 15:41:05 +0800

    test

--- demo-dao/src/test/java/com/devilmole/test/dao/test.java
+++ demo-dao/src/test/java/com/devilmole/test/dao/test.java
@@ -1,14 +1,12 @@
 package com.devilmole.test.dao;
 
-import com.devilmole.dao.UserMapper;
+
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 
 /**
  * Created by Administrator on 2016/2/19 0019.
  */
 public class test {
-
 
     @Test
     public void testName() throws Exception {
