292cd7d655cbb2270a751a7c55911a205ee644fd
devilmole <devilmole@gmail.com>
2016-02-22 17:13:53 +0800

    test

--- demo-dao/src/test/java/com/devilmole/test/dao/test.java
+++ demo-dao/src/test/java/com/devilmole/test/dao/test.java
@@ -1,15 +1,46 @@
 package com.devilmole.test.dao;
 
 
+import com.devilmole.dao.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Reader;
 
 /**
  * Created by Administrator on 2016/2/19 0019.
  */
 public class test {
 
+   private UserMapper userMap;
+
+    @Before
+    public void setUp()
+    {
+//        // create a SqlSessionFactory
+//        System.out.println("-------123-->");
+//        Reader reader = Resources.getResourceAsReader("conf/spring-mybatis.xml");
+//        //Reader reader = Resources.getResourceAsReader("conf/spring-config.xml");
+//        System.out.println(reader.toString());
+//        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+//        reader.close();
+        @SuppressWarnings("resource")
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:conf/spring-config.xml"
+                ,"classpath:conf/spring-mybatis.xml"});
+        userMap=(UserMapper)context.getBean("userMapper");
+
+    }
     @Test
-    public void testName() throws Exception {
+    public void testFunction(){
+        double result=userMap.getMaxNum();
+        System.out.println("result---------》"+result);
     }
 
 }
